ad4de3d8dda896693c427890921fb3584c1825a7
Joey Sabey <GameFreak7744@gmail.com>
2013-08-22 07:17:45 +0100

    Swapping an int-specific error message into input.Int() where it belongs, from input.Read()

--- input/input.go
+++ input/input.go
@@ -37,14 +37,14 @@ func Read(p string,
         if e = f(s, x); e == nil {
             break
         }
-        fmt.Println("Not an integer")
+        fmt.Println(e)
     }
 }
 //Function to parse ints, for use with input.Read()
 func Int(s string, x interface{}) error {
     i, e := strconv.Atoi(s)
     if e != nil {
-        return e
+        return fmt.Errorf("Not an integer")
     }
     *x.(*int) = i
     return nil
